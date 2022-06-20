6a39b0e17d98044f5525ad32b370c7c8b9f88abd
vsaceur <106846924+vsaceur@users.noreply.github.com>
2022-06-20 22:15:30 +0200

    Update sirius-server.py

--- sirius-server.py
+++ sirius-server.py
@@ -7,7 +7,7 @@ from re import findall
 import psutil
 import sys
 
-hostName = "sirius.vrdaf.org"
+hostName = "nameofserver"
 serverPort = 8081
 clientaddr =""
 
@@ -15,7 +15,7 @@ def publish_message(topic, message):
     #print("Publishing to MQTT topic: " + topic)
     #print("Message: " + message)
 
-    publish.single(topic, message, hostname="192.168.0.77")
+    publish.single(topic, message, hostname="IPADDR")
     
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -25,8 +25,8 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(bytes("<html><head><title>NATO SENSOR</title></head>", "utf-8"))
         self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is the NATO IDS.</p>", "utf-8"))
-        self.wfile.write(bytes("<p>You are not NATO.</p>", "utf-8"))
+        self.wfile.write(bytes("<p>This is an IDS.</p>", "utf-8"))
+        self.wfile.write(bytes("<p>You are not US.</p>", "utf-8"))
         self.wfile.write(bytes("<p>Launching Countermeasuers against %s.</p>"% self.address_string(), "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
         publish_message("M9",self.address_string())
